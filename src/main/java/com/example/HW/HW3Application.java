6dc87b1eb58f53b9a8b171d6539be59b2e403aaf
Stronfil <tambovskii99@gmail.com>
2021-06-02 17:06:37 +0300

    Initial commit

---
+++ src/main/java/com/example/HW/HW3Application.java
@@ -0,0 +1,15 @@
+package com.example.HW;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class HW3Application {
+
+	public static void main(String[] args) {
+		run(HW3Application.class, args);
+
+	}
+
+}
